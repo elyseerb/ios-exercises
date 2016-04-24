246449c99dc21b2fe0f67d013accc95897eeffe8
Elyse Erb <jerb@me.com>
2016-04-24 14:45:53 -0700

    Assignment 11 completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,15 +12,31 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
-
-    /* WORK HERE */
+    
+    if (dollars <= 4) {
+        NSString *itemToReturn = [NSString stringWithFormat:@"get out of my store"];
+        return itemToReturn;
+    } else if (dollars == 5) {
+        NSString *itemToReturn = [NSString stringWithFormat:@"have some gum"];
+        return itemToReturn;
+    } else if (dollars == 6) {
+        NSString *itemToReturn = [NSString stringWithFormat:@"have an apple"];
+        return itemToReturn;
+    } else if (dollars == 1000) {
+        NSString *itemToReturn = [NSString stringWithFormat:@"have an Apple computer"];
+        return itemToReturn;
+    } else if (dollars == 1000000000) {
+        NSString *itemToReturn = [NSString stringWithFormat:@"have The Big Apple"];
+        return itemToReturn;
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
+    
+    /* Not really sure what I should be adjusting here. Tests are passing. Should I have been looking into self.getsDiscount ? */
 
     NSUInteger cost = 24;
     
